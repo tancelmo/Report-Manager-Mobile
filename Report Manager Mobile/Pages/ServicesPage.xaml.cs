e5482062f86e0d73f2f92c731538e7857b0cd70e
Taliston Ancelmo da Silva <bakura@live.co.uk>
2023-08-03 22:51:02 -0300

    MailSend Test

--- Report Manager Mobile/Pages/ServicesPage.xaml.cs
+++ Report Manager Mobile/Pages/ServicesPage.xaml.cs
@@ -1,3 +1,6 @@
+using Report_Manager_Mobile.Resources.Languages;
+using System.Diagnostics;
+
 namespace Report_Manager_Mobile.Pages;
 
 public partial class ServicesPage : ContentPage
@@ -6,4 +9,42 @@ public partial class ServicesPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+
+        var contacts = new List<string>();
+        contacts.Add("tadasilv@accellsolutions.com");
+        
+        try
+        {
+            var message = new EmailMessage
+            {
+                Subject = "NS 7770022154 INST B14447899 ACCELL SOLUCOES PARA ENERGIA E AGUA",
+                Body = SearchBar.Text,
+                To = contacts,
+                Cc = contacts,
+                //Bcc = contacts
+            };
+            var fn = "Attachment.txt";
+            var file = Path.Combine(FileSystem.CacheDirectory, fn);
+            File.WriteAllText(file, "File inside" + SearchBar.Text);
+
+            message.Attachments.Add(new EmailAttachment(file));
+
+            await Email.ComposeAsync(message);
+            
+        }
+        catch (FeatureNotSupportedException fbsEx)
+        {
+            // Email is not supported on this device
+            await DisplayAlert(AppResource.WarnignCaption, fbsEx.Message, AppResource.OkButton);
+            Debug.WriteLine(fbsEx);
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert(AppResource.WarnignCaption, "UE", AppResource.OkButton);
+            // Some other exception occurred
+        }
+    }
 }
